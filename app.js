5ac7db24493ab653264c42046297488446721585
foxenko <ilyabro65410@gmail.com>
2024-04-28 21:39:15 +0500

    1

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-const express = require('express')
-const path = require('path')
-const bodyParser = require('body-parser')
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
 
-const gamesRouter = require('./routes/games')
-const categoriesRouter = require('./routes/categories')
-const usersRouter = require('./routes/users')
-// const connectToDatabase
+const gamesRouter = require("./routes/games");
+const categoriesRouter = require("./routes/categories");
+const usersRouter = require("./routes/users");
+const connectToDatabase = require("./database/connect");
+
+const PORT = 3000;
+
+const app = express();
+connectToDatabase();
+
+app.use(
+  bodyParser.json(),
+  express.static(path.join(__dirname, "public")),
+  usersRouter,
+  gamesRouter,
+  categoriesRouter
+);
+
+app.listen(PORT, () => {
+  console.log(`server on ${PORT}`);
+});
---
+++ database/connect.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const DB_URL = 'mongodb://localhost:27017/pindie'
+
+const connectToDatabase = async () => {
+    try {
+        await mongoose.connect
+    } catch (error) {
+        console.log(`DB connection error ${error}`)
+    }
+
+};
+
+moduke.exports = connectToDatabase;
