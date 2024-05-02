ccf683b40886461f4fdd401a94796f60c89dee32
foxenko <ilyabro65410@gmail.com>
2024-05-02 15:45:14 +0500

    add cors middleware

---
+++ middlewares/cors.js
@@ -0,0 +1,14 @@
+const allowedCors = [
+  "localhost:3000",
+  "https://practicum.yandex.ru",
+  "https://students-projects.ru",
+];
+cors = (req, res, next) => {
+  const { origin } = req.headers;
+  if (allowedCors.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+  next();
+};
+
+module.exports = cors;
