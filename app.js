7b990372fb77c7c6e8f31f95a39202926db080a0
foxenko <ilyabro65410@gmail.com>
2024-05-02 16:52:34 +0500

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const gamesRouter = require("./routes/games");
 const categoriesRouter = require("./routes/categories");
 
 const connectToDatabase = require("./database/connect");
-const cors = require('./middlewares/cors');
+// const cors = require('./middlewares/cors');
 
 const app = express();
 const PORT = 3000;
@@ -15,7 +15,7 @@ const PORT = 3000;
 connectToDatabase();
 
 app.use(
-  cors,
+  // cors,
   bodyParser.json(),
   express.static(path.join(__dirname, "public")),
   usersRouter,
