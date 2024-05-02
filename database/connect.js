d6712cf27b57b02e45f33665304f571af29631dc
foxenko <ilyabro65410@gmail.com>
2024-05-02 15:38:40 +0500

    Update connect.js

--- database/connect.js
+++ database/connect.js
@@ -1,14 +1,13 @@
 const mongoose = require("mongoose");
 
-const DB_URL = 'mongodb://localhost:27017/pindie'
+const DB_URL = "mongodb://localhost:27017/pindie";
 
 const connectToDatabase = async () => {
-    try {
-        await mongoose.connect
-    } catch (error) {
-        console.log(`DB connection error ${error}`)
-    }
-
+  try {
+    await mongoose.connect;
+  } catch (error) {
+    console.log(`DB connection error ${error}`);
+  }
 };
 
 module.exports = connectToDatabase;
