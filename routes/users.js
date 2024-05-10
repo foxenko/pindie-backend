fe67e79df375a83efe9916fae4ad5ff0ff906e0d
foxenko <ilyabro65410@gmail.com>
2024-05-10 16:43:30 +0500

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,9 @@ const {
   createUser,
   deleteUser,
   updateUser,
+  checkIsUserExists,
+  checkEmptyNameAndEmailAndPassword,
+  checkEmptyNameAndEmail,
 } = require("../middlewares/users");
 const {
   sendAllUsers,
@@ -16,8 +19,21 @@ const {
 
 usersRouter.get("/users", findAllUsers, sendAllUsers);
 usersRouter.get("/users/:id", findUserById, sendUserById);
-usersRouter.post("/users", findAllUsers, createUser, sendUserCreated);
-usersRouter.put("/users/:id", updateUser, sendUserUpdated);
+usersRouter.post(
+  "/users",
+  findAllUsers,
+  checkIsUserExists,
+  checkEmptyNameAndEmailAndPassword,
+  createUser,
+  sendUserCreated
+);
+
+usersRouter.put(
+  "/users/:id",
+  checkEmptyNameAndEmail,
+  updateUser,
+  sendUserUpdated
+);
 usersRouter.delete("/users/:id", deleteUser, sendUserDeleted);
 
 module.exports = usersRouter;
