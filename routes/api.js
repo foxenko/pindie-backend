a91f697f10fdeb0c88fbb2bfe5eba05ac9b4cfcc
foxenko <ilyabro65410@gmail.com>
2024-05-12 21:20:17 +0500

    update api.js

--- routes/api.js
+++ routes/api.js
@@ -7,6 +7,6 @@ const apiRouter = require("express").Router();
 apiRouter.use("/api", gamesRouter);
 apiRouter.use("/api", usersRouter);
 apiRouter.use("/api", categoriesRouter);
-apiRouter.use("/api", authRouter);
+// apiRouter.use("/api", authRouter);
 
 module.exports = apiRouter;
