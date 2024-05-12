cd3eba908d096c456198bad9a32df288d069b54b
foxenko <ilyabro65410@gmail.com>
2024-05-12 22:02:14 +0500

    update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,16 @@
 const users = require("../models/users");
+const jwt = require("jsonwebtoken");
 
 const login = (res, req) => {
   const { email, password } = req.body;
   users
     .findUserByCredentials(email, password)
+    .then((user) => {
+      const token = jwt.sign(email, "group26", {
+        expiresIn: 3600,
+      });
+      return { user, token };
+    })
     .then((user) => {
       res
         .status(200)
