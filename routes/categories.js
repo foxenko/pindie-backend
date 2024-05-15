4726d6e41b5c54baf50f5b826ffd0cecc00eef36
foxenko <ilyabro65410@gmail.com>
2024-05-15 22:31:26 +0500

    Update categories.js

--- routes/categories.js
+++ routes/categories.js
@@ -1,4 +1,3 @@
-const categoriesRouter = require("express").Router();
 const {
   findAllCategories,
   findCategoryById,
@@ -15,24 +14,38 @@ const {
   sendCategoryDeleted,
   sendCategoryUpdated,
 } = require("../controllers/categories");
+const { checkAuth } = require("../middlewares/auth");
+
+const categoriesRouter = require("express").Router();
 
 categoriesRouter.get("/categories", findAllCategories, sendAllCategories);
-categoriesRouter.get("/categories/:id", findCategoryById, sendCategoryById);
+categoriesRouter.get(
+  "/categories/:id",
+  findAllCategories,
+  findCategoryById,
+  sendCategoryById
+);
 categoriesRouter.post(
   "/categories",
   findAllCategories,
   checkIsCategoryExists,
   checkEmptyName,
+  checkAuth,
   createCategory,
   sendCategoryCreated
 );
-
 categoriesRouter.put(
   "/categories/:id",
   checkEmptyName,
+  checkAuth,
   updateCategory,
   sendCategoryUpdated
 );
-categoriesRouter.delete("/categories/:id", deleteCategory, sendCategoryDeleted);
+categoriesRouter.delete(
+  "/categories/:id",
+  checkAuth,
+  deleteCategory,
+  sendCategoryDeleted
+);
 
 module.exports = categoriesRouter;
