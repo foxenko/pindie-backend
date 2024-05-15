93f54708e98ee5a2a6043aac061b45902724c9f4
foxenko <ilyabro65410@gmail.com>
2024-05-15 22:26:22 +0500

    Update pages.js

--- routes/pages.js
+++ routes/pages.js
@@ -6,4 +6,4 @@ const pagesRouter = require("express").Router();
 pagesRouter.get("/admin/**", checkCookiesJWT, checkAuth, sendDashboard);
 pagesRouter.get("/", sendIndex);
 
-module.exports = { pagesRouter };
+module.exports = pagesRouter;
