41726896663af6e38edb9f6f876b3d18eae14586
foxenko <ilyabro65410@gmail.com>
2024-05-15 22:23:12 +0500

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,3 @@
-const usersRouter = require("express").Router();
-
 const {
   findAllUsers,
   findUserById,
@@ -21,25 +19,28 @@ const {
 } = require("../controllers/users");
 const { checkAuth } = require("../middlewares/auth");
 
+const usersRouter = require("express").Router();
+
 usersRouter.get("/users", findAllUsers, sendAllUsers);
-usersRouter.get("/users/:id", findUserById, sendUserById);
+usersRouter.get("/users/:id", findAllUsers, findUserById, sendUserById);
 usersRouter.get("/me", checkAuth, sendMe);
 usersRouter.post(
   "/users",
   findAllUsers,
   checkIsUserExists,
   checkEmptyNameAndEmailAndPassword,
+  checkAuth,
   hashPassword,
   createUser,
   sendUserCreated
 );
-
 usersRouter.put(
   "/users/:id",
   checkEmptyNameAndEmail,
+  checkAuth,
   updateUser,
   sendUserUpdated
 );
-usersRouter.delete("/users/:id", deleteUser, sendUserDeleted);
+usersRouter.delete("/users/:id", checkAuth, deleteUser, sendUserDeleted);
 
 module.exports = usersRouter;
