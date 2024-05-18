bf6ac40cd7f96a47320fc3d51f2f2ac18659a2dd
foxenko <ilyabro65410@gmail.com>
2024-05-19 02:40:52 +0500

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 const checkAuth = (req, res, next) => {
   const { authorization } = req.headers;
-  if (!authorization || !authorization.startWith("Bearer ")) {
+  if (!authorization || !authorization.startsWith("Bearer ")) {
     return res.status(401).send({ message: "Необходима авторизация" });
   }
   const token = authorization.replace("Bearer ", "");
