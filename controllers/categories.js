2df3143d7cb5a7cec9dc97f6b822fa2535bb656c
foxenko <ilyabro65410@gmail.com>
2024-05-19 02:29:52 +0500

    Update categories.js

--- controllers/categories.js
+++ controllers/categories.js
@@ -15,7 +15,7 @@ const sendCategoryCreated = (req, res) => {
 
 const sendCategoryUpdated = (req, res) => {
   res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify(req.category));
+  res.end(JSON.stringify({ message: "Категория обновлена" }));
 };
 const sendCategoryDeleted = (req, res) => {
   res.setHeader("Content-Type", "application/json");
