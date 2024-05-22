5fd99fcc55f489a25ce5b5a03007632ce590ab42
foxenko <ilyabro65410@gmail.com>
2024-05-23 00:59:56 +0500

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,10 @@
 const { login } = require("../controllers/auth");
 const { sendMe } = require("../controllers/users");
+const { checkAuth } = require("../middlewares/auth");
 
 const authRouter = require("express").Router();
 
 authRouter.post("/auth/login", login);
-// authRouter.get("/auth/me", Authorize, sendMe);
+authRouter.get("/auth/me", checkAuth, sendMe);
 
 module.exports = authRouter;
