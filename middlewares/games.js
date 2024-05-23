c300faecb0c83263ee0ec2cb3b3b5af1d61846ed
foxenko <ilyabro65410@gmail.com>
2024-05-23 18:26:54 +0500

    Update games.js

--- middlewares/games.js
+++ middlewares/games.js
@@ -79,7 +79,11 @@ const checkIsGameExists = async (req, res, next) => {
 };
 
 const checkIfCategoriesAvaliable = async (req, res, next) => {
-  if (!req.body.categories || req.body.categories.length === 0) {
+  if (
+    !req.body.categories ||
+    !req.isVoteRequest ||
+    req.body.categories.length === 0
+  ) {
     res.setHeader("Content-Type", "application/json");
     res
       .status(400)
